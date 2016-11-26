c3dd35712d6ed4be2c7a8e26ae98277f1077d3d4
Martin Jurča <mjurca@centrum.cz>
2016-11-26 13:11:07 +0100

    the count property now supports filtered ranges

--- range.js
+++ range.js
@@ -71,43 +71,82 @@ function range(start, end, step = null) {
       this._parentRange = parentRange
 
       /**
-       * @type {{next: function(): *}}
+       * @type {{next: function(): {done: boolean, value: *}}}
        */
       this._iterator = parentRange || createIterator(this)
+
+      /**
+       * @type {?number}
+       */
+      this._count = null
+
+      /**
+       * @type {[{done: boolean, value: *}, number][]}
+       */
+      this._preGeneratedValues = []
+
+      /**
+       * @type {number}
+       */
+      this._generatedValuesCount = 0
     }
 
     get count() {
-      if (!Number.isFinite(end)) {
-        return Number.POSITIVE_INFINITY
+      if (this._count !== null) {
+        return this._count
       }
 
-      if (!this._filter) {
-        return this._parentRange ?
+      if (!Number.isFinite(end)) {
+        let currentRange = this
+        let isFilteredRange = !!this._filter
+        while (!isFilteredRange && currentRange._parentRange) {
+          currentRange = currentRange._parentRange
+          isFilteredRange = !!currentRange._filter
+        }
+        if (isFilteredRange) {
+          console.warn(
+            'The count of filtered infinite ranges is always assumed to be ' +
+            'Infinity, even if that is not the case, because there is no ' +
+            'way to reliably determine if an infinitely-generated filtered ' +
+            'range would end up having a finite number of elements in a ' +
+            'finite time, given the fact that the filter is a callback.'
+          )
+        }
+
+        this._count = Number.POSITIVE_INFINITY
+      } else if (!this._filter) {
+        this._count = this._parentRange ?
             this._parentRange.count :
             Math.floor((end - start) / step)
+      } else {
+        let nextIteration = getNextValue(this, this._index)
+        while (!nextIteration[0].done) {
+          this._preGeneratedValues.push(nextIteration)
+          nextIteration = getNextValue(this, nextIteration[1])
+        }
+        this._count =
+            this._generatedValuesCount + this._preGeneratedValues.length
       }
 
-      // TODO: we have a filter
+      return this._count
     }
 
+    /**
+     * @return {{done: boolean, value: *}}
+     */
     next() {
-      let value = this._iterator.next()
-      let nextIndex
-      if (this._index < Number.MAX_SAFE_INTEGER) {
-        nextIndex = this._index + 1
+      let iteration, nextIndex
+      if (this._preGeneratedValues.length) {
+        [iteration, nextIndex] = this._preGeneratedValues.shift()
       } else {
-        nextIndex = Number.POSITIVE_INFINITY
-      }
-
-      if (this._filter && !this._filter(value, nextIndex, this)) {
-        return this.next()
+        [iteration, nextIndex] = getNextValue(this, this._index)
       }
 
       this._index = nextIndex
-      this._currentValue = this._map ?
-          this._map(value, this._index, this) :
-          value
-      return this._currentValue
+      this._currentValue = iteration.value
+      this._generatedValuesCount++
+
+      return iteration
     }
 
     enumerate() {
@@ -170,14 +209,52 @@ function range(start, end, step = null) {
       if (!this._parentRange) {
         clone._iterator = createIterator(clone)
       }
+      clone._count = this._count
+      clone._preGeneratedValues = this._preGeneratedValues.slice()
+      clone._generatedValuesCount = this._generatedValuesCount
       return clone
     }
 
+    /**
+     * @return {Range}
+     */
     [Symbol.iterator]() {
       return this
     }
   }
 
+  /**
+   * @param {Range} range
+   * @param {number} currentIndex
+   * @return {[{done: boolean, value: *}, number]}
+   */
+  function getNextValue(range, currentIndex) {
+    let iteration = range._iterator.next()
+    let nextIndex
+    if (currentIndex < Number.MAX_SAFE_INTEGER) {
+      nextIndex = currentIndex + 1
+    } else {
+      nextIndex = Number.POSITIVE_INFINITY
+    }
+
+    if (iteration.done) {
+      return [iteration, nextIndex]
+    }
+
+    if (range._filter && !range._filter(iteration.value, nextIndex, range)) {
+      return getNextValue(range, currentIndex)
+    }
+
+    if (range._map) {
+      iteration.value = range._map(iteration.value, nextIndex, range)
+    }
+    return [iteration, nextIndex]
+  }
+
+  /**
+   * @param {Range} rangeInstance
+   * @return {{next: function(): {done: boolean, value: *}}}
+   */
   function createIterator(rangeInstance) {
     return (function * () {
       while (!isEnded()) {
