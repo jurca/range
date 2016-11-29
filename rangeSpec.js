151a0318aa2e99da8d9297ae18aa2f2c926282ab
Martin Jurča <mjurca@centrum.cz>
2016-11-29 14:46:08 +0100

    added more tests for numeric sequences

--- rangeSpec.js
+++ rangeSpec.js
@@ -15,5 +15,31 @@ describe('range', () => {
     expect(testingRange.next()).toEqual({ done: false, value: 0 })
     expect(testingRange.next()).toEqual({ done: true, value: undefined })
   })
-  
+
+  it('should allow generating an incrementing numeric sequence', () => {
+    expect([...range(0, 10)]).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+  })
+
+  it('should allow generating a decrementing numeric sequence', () => {
+    expect([...range(10, 0)]).toEqual([10, 9, 8, 7, 6, 5, 4, 3, 2, 1])
+  })
+
+  it('should allow generating a single-element sequences', () => {
+    expect([...range(0, 1)]).toEqual([0])
+    expect([...range(7, 8)]).toEqual([7])
+  })
+
+  it('should allow generating empty sequences', () => {
+    expect([...range(0, 0)]).toEqual([])
+    expect([...range(4, 4)]).toEqual([])
+  })
+
+  it('should allow specifying the step size of the sequence', () => {
+    expect([...range(0, 10, 3)]).toEqual([0, 3, 6, 9])
+    expect([...range(0, 9, 3)]).toEqual([0, 3, 6])
+    expect([...range(0, 9, -3)]).toEqual([])
+    expect([...range(10, 0, -3)]).toEqual([10, 7, 4, 1])
+    expect([...range(10, 1, -3)]).toEqual([10, 7, 4])
+  })
+
 })
