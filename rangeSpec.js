88efacd367c77f534b90d08feab81cb21cad7e2b
Martin Jurča <mjurca@centrum.cz>
2016-11-30 20:15:39 +0100

    added a test for mapping the sequence

--- rangeSpec.js
+++ rangeSpec.js
@@ -73,4 +73,24 @@ describe('range', () => {
     expect(range(-100, -Infinity).count).toBe(Infinity)
   })
 
+  it('should support mapping the sequence values', () => {
+    let sourceRange = range(0, 5)
+    let testingRange = sourceRange.map((value, index, rangeInstance) => {
+      expect(rangeInstance).toBe(testingRange)
+      return { index: -index, value: value * 2 }
+    })
+    expect(testingRange).not.toBe(sourceRange)
+    expect([...testingRange]).toEqual([
+      { index: -1, value: 0 },
+      { index: -2, value: 2 },
+      { index: -3, value: 4 },
+      { index: -4, value: 6 },
+      { index: -5, value: 8 }
+    ])
+    expect(sourceRange.next()).toEqual({
+      done: true,
+      value: undefined
+    })
+  })
+
 })
